abb9374ccef9867996facc05c319f23c312c113b
DisSys <20084833@mail.wit.ie>
2023-10-18 18:46:34 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -33,11 +33,16 @@ function addColleague(cs : Colleague[], name : string, department : string, emai
 
 function sortColleagues(
   colleagues: Colleague[],
-  sorter: (c1: Colleague, c2: Colleague) => number
+  sorter: (c1: Colleague, c2: Colleague) => number,
+  max? : number
 ): EmailContact[] {
-  const sorted = colleagues.sort(sorter); // Colleague[] inferred
-  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
-  return result 
+  let end = colleagues.length;
+  if (max !== undefined) {
+     end = max < 2 ? 1 : max
+  }
+  const sorted = colleagues.sort(sorter);
+  const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return fullResult.slice(0,end)
 }
 
 function findFriends(
@@ -46,12 +51,22 @@ function findFriends(
   return friendsArray.filter(callback).map(friend => friend.name);
   }
 
+  function addInterest(friend: Friend, interest: string): string[] {
+    if (!friend.interests) {
+      friend.interests = [];
+    }
+    friend.interests.push(interest);
+    return friend.interests;
+  }
+
 console.log(older(friends[0]))
 console.log(allOlder(friends));
 console.log(highestExtension(colleagues.current));
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
-console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
-console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension), 3));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length)));
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+console.log(addInterest(friends[1], 'Politics'))
